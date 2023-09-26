c5337dc5c72fdeab674cea53d62a226e1b5ed3f2
Yuluki <web.yulukitaiga@gmail.com>
2023-09-26 16:06:49 +0900

    (*Commented out because it is a Docker environment) Added as a comment the setting to run the System Spec of Home in Headless mode for the local environment

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Homes', type: :system do
   before do
     driven_by(:rack_test)
+    # driven_by :selenium_chrome_headless # ヘッドレスモードで実行に変更（Docker環境なので設定不必要）
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}"
