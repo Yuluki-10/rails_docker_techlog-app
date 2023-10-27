4f5b5c02336ccb82a232ad284e809a02f4d591d6
Yuluki <web.yulukitaiga@gmail.com>
2023-10-27 14:55:48 +0900

    Delete　features related to Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'home#top'
+  root 'posts#index'
 
-  resources :posts, only: [:index, :new, :create, :show, :destroy]
+  resources :posts, only: [:new, :create, :show, :destroy]
 end
