f04dd160c722b7b595da74dd3e3c515493505e45
Yuluki <web.yulukitaiga@gmail.com>
2023-11-06 20:22:35 +0900

    修正

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ "${RAILS_ENV}" = "production" ]
-then
-    bundle exec puma -C config/puma.rb # 本番環境（Heroku）では、Puma起動
-else
-    rails server -b 0.0.0.0 -p 3000 # 開発環境ではrails serverコマンド（と言っても、Rails5以降はPuma起動になる?）
-fi
+# if [ "${RAILS_ENV}" = "production" ]
+# then
+#     bundle exec puma -C config/puma.rb # 本番環境（Heroku）では、Puma起動
+# else
+#     rails server -b 0.0.0.0 -p 3000 # 開発環境ではrails serverコマンド（と言っても、Rails5以降はPuma起動になる?）
+# fi
+
+rails server -b 0.0.0.0 -p 3000
