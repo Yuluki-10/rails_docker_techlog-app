b0811e567febb215beac1cb68f9d1bf22f810f04
Yuluki <web.yulukitaiga@gmail.com>
2023-11-06 17:22:34 +0900

    コンテナ起動時コマンドを環境によって分けてみる

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 if [ "${RAILS_ENV}" = "production" ]
 then
-    bundle exec puma -C config/puma.rb
+    # bundle exec puma -C config/puma.rb
+    rails server -b 0.0.0.0 -p 3000
 else
     rails server -b 0.0.0.0 -p 3000
 fi
