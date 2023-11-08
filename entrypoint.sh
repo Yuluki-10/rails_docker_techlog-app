eab536579ab0f47965331a7baa763cbbfddefacc
Yuluki <web.yulukitaiga@gmail.com>
2023-11-08 19:32:46 +0900

    Procfileが効かないので、削除。一旦終了

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,4 +12,4 @@ fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 # コンテナのプロセスを実行。`exec "$@"`で、Dockerfile 内の`CMD ["rails", "server", "-b", "0.0.0.0"]`を実行。(=`rails s`)
-# exec "$@"
+exec "$@"
