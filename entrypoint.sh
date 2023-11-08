41272caebf36211463813cd0566209a0b32a75b0
Yuluki <web.yulukitaiga@gmail.com>
2023-11-08 14:25:27 +0900

    本番環境でtailwindビルド

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,10 @@ rm -f /rails_docker_techlog-app/tmp/pids/server.pid
 # 追記
 if [ $RAILS_ENV = 'production' ]; then
   # bundle exec rails assets:clobber
+  rails tmp:cache:clear
   bundle exec rails assets:precompile
-  # bundle exec rails tailwindcss:build
+  bundle exec rails assets:clean
+  bundle exec rails tailwindcss:build
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
